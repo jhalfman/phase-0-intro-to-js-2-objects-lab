84d3134098079ce26d593892306f74b2fa9211c7
Jordan Halfman <jordanhalfman@gmail.com>
2022-05-02 16:24:40 -0700

    commit tree 90f28f96d3b9cc70c1973f4e3e421b49ce230fbf

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+    name: "Jordan",
+    streetAddress: "here it is",
+}
+
+function updateEmployeeWithKeyAndValue(emp, key, value){
+    return {
+    ...emp,
+    [key]: value,
+    }
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(emp, key, value){
+    emp[key] = value;
+    return emp;
+}
+
+function deleteFromEmployeeByKey(emp, key){
+    const newEmp = {...emp};
+    delete newEmp[key];
+    return newEmp;
+}
+
+function destructivelyDeleteFromEmployeeByKey(emp, key){
+    delete emp[key];
+    return emp;
+}
